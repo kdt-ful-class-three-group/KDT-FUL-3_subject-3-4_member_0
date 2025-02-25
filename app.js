64b45528965fef4e2b7a47248201c9944454bfa1
kongukjae <kongukjae@gmail.com>
2025-02-25 12:56:30 +0900

    app.js에 정적 파일 응답 처리 추가 및 CSS 파일 색상 수정

--- app.js
+++ app.js
@@ -23,6 +23,30 @@ const server = http.createServer((request, response) => {
 
       response.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
       response.end(html);
+
+      //* 아래는 js, css 파일 응답 코드
+
+
+      /**
+       * TODO : 생각해볼 요소
+       * 
+       * ? 몇개의 정적 페이지를 불러들이고 요청해야될지 미지수이니까,
+       * ? 이 코드의 if문 응답은 적절하다고 볼 수 없음
+       * 
+       * * 추후에 규모가 커질 것을 생각하면 어떻게 처리해야 할까?
+       * 
+       * 
+       */
+
+
+    } else if (request.url === '/index.js') {
+      const js = fs.readFileSync('./public/index.js', 'utf-8');
+      response.writeHead(200, { 'Content-Type': 'application/javascript; charset=utf-8' });
+      response.end(js);
+    } else if (request.url === '/index.css') {
+      const css = fs.readFileSync('./public/index.css', 'utf-8');
+      response.writeHead(200, { 'Content-Type': 'text/css; charset=utf-8' });
+      response.end(css);
     }
   } 
   
