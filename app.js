54deadb9e497610196bd8ad4c120c04e786953c3
kongukjae <kongukjae@gmail.com>
2025-02-25 12:49:04 +0900

    app.js에 주석 추가: 특정 URL 요청 시 파일 읽기 여부에 대한 TODO 및 질문 포함

--- app.js
+++ app.js
@@ -8,6 +8,15 @@ const server = http.createServer((request, response) => {
 
   if (request.method === 'GET') {
     if (request.url === '/') {
+
+      // TODO : 생각해볼 요소
+
+      /**
+       * ? 특정 url이 요청 들어롤때 file을 읽어오는 것이 좋을까?
+       * 
+       */
+
+
       console.log(" '/' 요청에, /public/index.html 파일 응답해주기");
 
       const html = fs.readFileSync('./public/index.html', 'utf-8');
